71e9cf28d369f1ddd2816ffb221f2a57f7a129b3
attilathedud <attilathedud@gmail.com>
2021-05-15 13:58:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
+/*
+	A hack for Assault Cube that removes all recoil when firing a weapon.
+	
+	This is done by modifying the game's code responsible for setting the player's recoil and changing the final instruction
+	- which changes the value of the player's yaw - to instead pop a value that is ignored. As such, the player's yaw is
+	never modified.
+
+	This must be injected into the Wesnoth process to work. One way to do this is to use a DLL injector. 
+	Another way is to enable AppInit_DLLs in the registry.
+	
+	The offsets and method to discover them are discussed in the article at: https://gamehacking.academy/lesson/36
+*/
 #include <Windows.h>
 
 // The new opcodes to write into the game's code
 unsigned char new_bytes[3] = { 0xDD, 0xD8, 0x90 };
 
+// When our DLL is attached, first unprotect the memory responsible for adding recoil in the game
+// Then, write our new opcodes into that memory location
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 	DWORD old_protect;
 	unsigned char* hook_location = (unsigned char*)0x45BAAD;
